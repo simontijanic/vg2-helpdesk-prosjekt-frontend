a247dd38283f044b3b9b2794651f2d840e3b7ce1
simontijanic <simontijanicerkul@gmail.com>
2025-04-08 16:04:32 +0200

    endring på ratelimiting litt

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,13 +1,13 @@
 const rateLimit = require('express-rate-limit');
 
 const generalLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutter
+    windowMs: 5 * 60 * 1000, // 15 minutter
     max: 100,
     message: 'For mange forespørsler. Prøv igjen senere.',
 });
 
 const authLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutter
+    windowMs: 5 * 60 * 1000, // 15 minutter
     max: 10, // Maks 10 forespørsler
     message: 'For mange innloggingsforsøk. Prøv igjen senere.',
 });
