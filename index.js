9fc195779eefa2f058e117a60b911203913ec403
simontijanic <simontijanicerkul@gmail.com>
2025-04-08 15:48:51 +0200

    cors added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 const { generalLimiter, authLimiter } = require('./middleware/rateLimiter');
 app.use(generalLimiter);
